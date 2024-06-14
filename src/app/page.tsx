c0e8751ede99ca56fae4ab43a11bd15dadbc9ab0
Alejo <39176926+imalegonzalez@users.noreply.github.com>
2024-06-14 04:52:01 -0300

    Actualizar page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,24 @@
-import Image from "next/image";
-import {client} from "./lib/contentful/client"
+import React from "react";
+import { client } from "./lib/contentful/client";
 import InfiniteScroll from "./components/InfiniteScroll";
-import CardObra from './components/CardObra';
+import CardObra from "./components/CardObra";
 
-
-// posts will be populated at build time by getStaticProps()
-export default async function Home() {
-  const data = await getData();
-  // console.log(data[1].fields)
-
- async function getData() {
-  const res = await client.getEntries({ content_type: 'obra' })
-  return res.items
+async function fetchData() {
+  const res = await client.getEntries({ content_type: "obra" });
+  return res.items;
 }
+
+export default async function Home() {
+  const data = await fetchData();
 
   return (
-    <div className=" px-1 w-full  ">
-      <h2 className=" text-2xl my-3 font-bold ">home</h2>
-      
+    <div className="px-1 w-full">
+      <h2 className="text-2xl my-3 font-bold">home</h2>
+      {data.map((item, index) => (
+        <div key={index} className="my-4">
+          <CardObra obra={item} />
+        </div>
+      ))}
     </div>
-  )
+  );
 }
- 
