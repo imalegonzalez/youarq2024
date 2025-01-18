4d37558fbdd5c32c46ec0ae8e1c2826f697942a7
Alejo <imalegonzalez@gmail.com>
2025-01-18 18:55:46 -0300

    fixing icon types

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,7 @@ import Footer from "./components/Footer";
 import { Toaster } from "@/components/ui/toaster";
 import WhatsappLink from "./components/WhatsappLink";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faWhatsapp } from "@fortawesome/free-brands-svg-icons";
+import { faWhatsapp as faWhatsappIcon } from "@fortawesome/free-brands-svg-icons";
 
 
 
@@ -33,7 +33,7 @@ export default function RootLayout({
         <Footer/>
     
           <WhatsappLink visible={false} className="h-16 w-16 fixed bottom-5 right-5 md:bottom-10 md:right-10 z-50 bg-green-500 rounded-full p-4 flex justify-center items-center shadow-lg shadow-green-500/50 hover:bg-green-400 transition-all duration-200 hover:scale-105  animate-scale-in-bounce">
-            <FontAwesomeIcon icon={faWhatsapp} className="text-white text-xl" />
+            <FontAwesomeIcon icon={faWhatsappIcon as any} className="text-white text-xl" />
           </WhatsappLink>
      
       </body>
